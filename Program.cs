a8b0d0a0d1b79afb17c7e37f2a473893eaf1fe2f
Ahmed_Hesham <ahmed.hesham151200@gmail.com>
2024-11-14 17:38:35 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_1
+{
+    internal class Task
+    {
+        static void Main(string[] args)
+        {
+            int X = 10;
+            int Y = 20;
+            Console.WriteLine($"Equation: {X} + {Y} = {X + Y:C}");
+        }
+    }
+}
